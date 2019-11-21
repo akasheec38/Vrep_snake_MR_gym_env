9420fbd16b4c2a825b6a547e51d5f64e166c5dc7
Akash Singh <38921258+akasheec38@users.noreply.github.com>
2019-11-21 22:36:12 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+
+from setuptools import setup
+
+setup(name='Snake_MR',
+      version='0.0.1',
+      description='OpenAI Gym environment for VREP Modular Snake Robot',
+      author='Akash Singh',
+      install_requires=['gym', 'numpy'],
+      python_requires='>=3.6'
+)
